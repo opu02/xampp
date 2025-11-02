5f70cd2003aea0317a70040309d259d32aa9e2d7
opu02 <opu02@gmail.com>
2025-11-02 12:17:23 +0600

    added a form

---
+++ Lab_task-1.php
@@ -0,0 +1,89 @@
+<html>
+<head>
+    
+    <title>Form</title>
+    
+</head>
+<style>
+        .btn{
+          color:white;
+          border: 2px solid;
+          padding: 2px;
+          width: 200px;
+          height: 50px;
+          
+        }
+    </style>
+<body>
+  
+  <center>
+    <h2 style="color:blueviolet">Clinic Patient Registration</h2>
+  </center>
+
+  <center>
+    <form>
+    <b>Full Name:</b>
+    <br>
+    <input type="text">
+    <br>
+    <br>
+
+    <b>Age:</b>
+    <br>
+    <input type="text">
+    <br>
+    <br>
+    <b>Phone Number:</b>
+    <br>
+    <input type="text">
+    <br>
+    <br>
+    <b>Email Address:</b>
+    <br>
+    <input type="email">
+    <br>
+    <br>
+
+    <b>Insurance Provider:</b>
+    <br>
+    <br>
+    <select>
+        <option>Select Provider</option>
+        <option>DBL</option>
+        <option>BBL</option>
+    </select>
+    <br>
+    <br>
+    <b>Insurance Policy Number:</b>
+    <br>
+    <input type="text">
+
+    <center>
+    <h2 style="color:blueviolet">Additional Information</h2>
+  </center>
+  <center>
+    <b>Username:</b>
+    <br>
+    <input type="text">
+    <br>
+    <br>
+    <b>Password:</b>
+    <br>
+    <input type="password">
+    <br>
+    <br>
+    <b>Confirm Password:</b>
+    <br>
+    <input type="password">
+    <br>
+    <br>
+
+    <button class="btn" style="background-color:blueviolet"><b>Register</b></button>
+  </center>
+  </form>
+
+  </center>
+
+    
+</body>
+</html>
